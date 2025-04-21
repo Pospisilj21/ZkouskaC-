290b66624e133889f42dd65bfad813bf4d037cb5
Jan Pospíšil <pospisilj21@spstrutnov.cz>
2025-04-21 18:25:37 +0200

    Struktura pro přihlášení a registraci

--- Csharp/Csharp/Controllers/HomeController.cs
+++ Csharp/Csharp/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
 
-using Microsoft.AspNetCore.Mvc;
-
-namespace Csharp.Controllers
+namespace _10_ASP_NET_Core.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            ViewData["Titulek"] = "Hlavn� str�nka";
+
             return View();
         }
     }
---
+++ Csharp/Csharp/Controllers/UzivatelController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace maturitaZkouska.Controllers
+{
+    public class UzivatelController : Controller
+    {
+        [HttpGet]
+        public IActionResult Registrovat()
+        {
+            ViewData["Titulek"] = "Registrace";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrovat(string jmeno, string heslo, string hesloKontrola)
+        {
+            return RedirectToAction("Prihlasit");
+        }
+
+        public IActionResult Prihlasit()
+        {
+            ViewData["Titulek"] = "Přihlášení";
+
+            return View();
+        }
+    }
+}
